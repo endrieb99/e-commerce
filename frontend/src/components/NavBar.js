af3aec431d8d6e35059c0f4e3d97e94d7740a2c7
Endri Bida <bida.endri1@gmail.com>
2021-07-03 00:54:07 +0200

    unordered list element

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,14 +1,47 @@
 import React from 'react'
 import {Link, NavLink} from 'react-router-dom';
 
-const NavBar = ({}) => {
+
+const NavBar = ({history}) => {
+    // //Burger
+    // const Buric = useRef(null)
+    // const navLinks = useRef(null)
+    // const rightItems = useRef(null)
+
+    // const onBurgActive = () =>{
+    //     //Toggle Nav
+
+    //     const links = document.querySelectorAll('.navLinks li')
+    //     navLinks.current.classList.toggle('burgerActive')
+    //     rightItems.current.classList.toggle('burgerActive')
+    //     //Animate Links
+    //     links.forEach((link,index) => {
+    //      if(link.style.animation)
+    //           {
+    //                 link.style.animation = "";
+    //                 rightItems.current.style.animation = "";
+    //            }
+    //         else 
+    //         { 
+                   
+    //                 link.style.animation = `moving 0.5s ease forwards ${index / 5 }s`
+    //                 rightItems.current.style.animation = `moving 0.5s ease forwards ${index / 5 }s`
+                   
+    //             }
+    //     });
+    //     //Burger Animation
+    //     Buric.current.classList.toggle('toggle')
+    // }
+
     return (
         <nav className="nav" >
         <div className="logo"><Link to =''>ALBANIA MARKET</Link></div>
+        <ul className="navLinks">
         <NavLink to="/" exact  activeClassName='activlink' ><li>Home</li></NavLink>
             <NavLink to="/shop" activeClassName='activlink' ><li>Shop</li></NavLink>
             <NavLink to="/contact"activeClassName='activlink' ><li>Contact us</li></NavLink>
             <NavLink to="/about" activeClassName='activlink'><li>About</li></NavLink> 
+        </ul>
         </nav> 
     )
 }
