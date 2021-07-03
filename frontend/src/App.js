0a992436da566bb9f4fbddb79d2751f8ed067832
Dorian Kamberi <dorian.kamberi@epitech.eu>
2021-07-03 21:23:54 +0200

    some changes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,46 @@
 import NavBar from './components/NavBar';
 import ScrollIntoView from './components/Scrollinintoview'
-import Home from './pages/Home'
-import Contact from './pages/Contact/Contact'
-import React, {useState,useEffect} from 'react'
-import {BrowserRouter as Router , Switch ,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Contact from './Pages/Contact/Contact'
+import Footer from './Pages/Footer/Footer'
+import About from './Pages/About/About'
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ChakraProvider } from "@chakra-ui/react"
 
-const App = () => { 
-  const  [loading,setLoading] = useState(false)
+const App = () => {
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
     setLoading(true)
-    setTimeout( ()=> {
+    setTimeout(() => {
       setLoading(false)
-    },3000)
+    }, 3000)
 
   }, [])
 
   return (
-    <div className = 'main'>
-<ChakraProvider>
-       <Router>
-         <ScrollIntoView>
-         {
-         <>
-                 <NavBar/>
-                 <Switch>  
-                 <Route path="/" exact component={Home}/>            
-                 <Route path="/contact" component={Contact}/>
-                 </Switch>
-          </>
-         }
-        </ScrollIntoView>
-      </Router>
-   </ChakraProvider>
+    <div className='main'>
+      <ChakraProvider>
+        <Router>
+          <ScrollIntoView>
+            {
+              <>
+                <NavBar />
+                <Switch>
+                  <Route path="/" exact component={Home} />
+                  <Route path="/contact" component={Contact} />
+                  <Route path="/About" component={About} />
+                  <Route path="/Footer" component={Footer} />
+
+                </Switch>
+              </>
+            }
+          </ScrollIntoView>
+        </Router>
+      </ChakraProvider>
     </div>
-     
-    
+
+
   )
 }
 export default App;
