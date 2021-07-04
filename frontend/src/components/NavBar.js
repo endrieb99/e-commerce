530a1d68bde3e8c42f8b2392cec6e6db1fe17fd9
Dorian Kamberi <dorian.kamberi@epitech.eu>
2021-07-04 23:06:19 +0200

    Navbar changes 2

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,6 +3,34 @@ import { Link, NavLink } from 'react-router-dom';
 
 
 const NavBar = ({ history }) => {
+    //Burger
+    const Buric = useRef(null)
+    const navLinks = useRef(null)
+    const rightItems = useRef(null)
+
+    const onBurgActive = () => {
+        //Toggle Nav
+
+        const links = document.querySelectorAll('.navLinks li')
+        // navLinks.current.classList.toggle('burgerActive')
+        rightItems.current.classList.toggle('burgerActive')
+        //Animate Links
+        links.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+                rightItems.current.style.animation = "";
+            }
+            else {
+
+                link.style.animation = `moving 0.5s ease forwards ${index / 5}s`
+                rightItems.current.style.animation = `moving 0.5s ease forwards ${index / 5}s`
+
+            }
+        });
+        //Burger Animation
+        Buric.current.classList.toggle('toggle')
+    }
+
     return (
         <nav className="nav" >
             <div className="logo"><Link to=''>ALBANIA MARKET</Link></div>
@@ -12,6 +40,13 @@ const NavBar = ({ history }) => {
                 <NavLink to="/contact" activeClassName='activlink' ><li>Contact us</li></NavLink>
                 <NavLink to="/about" activeClassName='activlink'><li>About</li></NavLink>
             </ul>
+            <div className="burger" ref={Buric} onClick={onBurgActive}>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='line3'></div>
+            </div>
+            <div className="rightComp" ref={rightItems}>
+            </div>
         </nav>
     )
 }
