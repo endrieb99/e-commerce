84cd5824a18b4d88f30ed10f05f73d77a22e3763
Dorian Kamberi <dorian.kamberi@epitech.eu>
2021-07-04 22:29:54 +0200

    Navbar changes

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import {Link, NavLink} from 'react-router-dom';
+import React, { useRef, useState, useEffect } from 'react'
+import { Link, NavLink } from 'react-router-dom';
 
 
-const NavBar = ({history}) => {
-  
+const NavBar = ({ history }) => {
     return (
         <nav className="nav" >
-        <div className="logo"><Link to =''>ALBANIA MARKET</Link></div>
-        <ul className="navLinks">
-        <NavLink to="/" exact  activeClassName='activlink' ><li>Home</li></NavLink>
-            <NavLink to="/shop" activeClassName='activlink' ><li>Shop</li></NavLink>
-            <NavLink to="/contact"activeClassName='activlink' ><li>Contact us</li></NavLink>
-            <NavLink to="/about" activeClassName='activlink'><li>About</li></NavLink> 
-        </ul>
-        </nav> 
+            <div className="logo"><Link to=''>ALBANIA MARKET</Link></div>
+            <ul className="navLinks">
+                <NavLink to="/" exact activeClassName='activlink' ><li>Home</li></NavLink>
+                <NavLink to="/shop" activeClassName='activlink' ><li>Shop</li></NavLink>
+                <NavLink to="/contact" activeClassName='activlink' ><li>Contact us</li></NavLink>
+                <NavLink to="/about" activeClassName='activlink'><li>About</li></NavLink>
+            </ul>
+        </nav>
     )
 }
 
